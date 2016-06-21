1940a8a70b29f265927c36383fdef4197875e8eb
sgr-ksmt <melodydance.k.s@gmail.com>
2016-06-21 18:57:13 +0900

    Create DPIType

---
+++ PDFGenerator/DPIType.swift
@@ -0,0 +1,28 @@
+//
+//  DPIType.swift
+//  PDFGenerator
+//
+//  Created by Suguru Kishimoto on 2016/06/21.
+//
+//
+
+import Foundation
+import UIKit
+
+public enum DPIType {
+    private static let defaultDpi: CGFloat = 72.0
+    case Default
+    case DPI_300
+    case Custom(CGFloat)
+    
+    public var value: CGFloat {
+        switch self {
+        case .Default:
+            return self.dynamicType.defaultDpi
+        case .DPI_300:
+            return 300.0
+        case .Custom(let value):
+            return value
+        }
+    }
+}
