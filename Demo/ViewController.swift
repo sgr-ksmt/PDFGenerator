0aa514ad77ff182d8360ea54c5321916ee5a2f6b
sgr-ksmt <melodydance.k.s@gmail.com>
2016-07-11 00:43:27 +0900

    Fix demo: add password example

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -66,7 +66,7 @@ class ViewController: UIViewController {
                     let data = try PDFGenerator.generate(images)
                     data.writeToFile(dst, atomically: true)
                 } else {
-                    try PDFGenerator.generate(images, outputPath: dst, dpi: .Custom(144))
+                    try PDFGenerator.generate(images, outputPath: dst, dpi: .Custom(144), password: "123456")
                 }
                 openPDFViewer(dst)
             } catch (let e) {
